16eb186f10a9596c1b59d7cedef9b3d296139f6f
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-04-03 22:07:28 +0900

    added plugins
    new file:   .gitignore
    new file:   ContestAppletProd.jnlp
    new file:   Plugin/moj/CodeProcessor.jar
    new file:   Plugin/moj/FileEdit.jar
    new file:   Plugin/moj/moj.jar
    new file:   Plugin/moj/moj_4.18.zip
    new file:   Plugin/moj/moj_instructions.txt
    new file:   Plugin/moj/template.cpp
    new file:   Plugin/moj/template.java
    new file:   contestapplet.conf

---
+++ Plugin/moj/template.java
@@ -0,0 +1,20 @@
+$BEGINCUT$
+$PROBLEMDESC$
+$ENDCUT$
+
+import java.util.*;
+
+public class $CLASSNAME$ {
+
+  public $RC$ $METHODNAME$($METHODPARMS$) {
+
+  }
+
+  $BEGINCUT$
+  $DEFAULTMAIN$
+  $ENDCUT$
+}
+
+$BEGINCUT$
+$TESTCODE$
+$ENDCUT$
